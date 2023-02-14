77d685658689589ccc750b971e42d3d754cf3f29
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-02-14 19:53:57 +0300

    add_home_page

--- butcher_shop/butcher_shop/settings.py
+++ butcher_shop/butcher_shop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'butcher_shop_app',
+    'recipes',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- butcher_shop/butcher_shop_app/views.py
+++ butcher_shop/butcher_shop_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,"butcher_shop_app/index.html")
+
